3e36aeb4fd7ed978e253e19bb7548ee311586aeb
Freedom Mubanga <francis_mubanga@yahoo.com>
2023-10-27 07:56:05 +0200

    -Almost Last Commmit if not last

--- Model/PriorityModel.cs
+++ Model/PriorityModel.cs
@@ -12,6 +12,25 @@ namespace TaskManagementSystem.Model
         public string priority_name { get; set; }
 
         [Required(ErrorMessage ="Please Ente A number to Set as Priority Level"),Range(1,5,ErrorMessage ="Please Enter a Number with the Ranges of 1 to 5 and only whole number are allowed")]
+        
         public int priority_level { get; set; } 
+    }
+    public class  UniqueInteger: ValidationAttribute
+    {
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            if(value is int intValue)
+            {
+                var instance = validationContext.ObjectInstance;
+                var property = validationContext.ObjectType.GetProperty(validationContext.MemberName);
+
+                var currentValue = (int)property.GetValue(instance);
+                if(intValue == currentValue)
+                {
+                    return new ValidationResult(ErrorMessage);
+                }
+            }
+            return ValidationResult.Success;
+        }
     }
 }
--- Model/TaskModel.cs
+++ Model/TaskModel.cs
@@ -11,11 +11,24 @@ namespace TaskManagementSystem.Model
         public string task_name { get; set; }
         [Required(ErrorMessage ="Task Description is required"),MinLength(10,ErrorMessage ="Length must be atleast 10 characters")]
         public string? task_description { get; set;}
-        [Required, Range(typeof(DateTime), "25/10/2023", "01/01/2060")]
+        /*[Required, Range(typeof(DateTime), "25/10/2023", "01/01/2060")]
+        public DateTime due_date { get; set; }*/
+        [Required]
+        [CustomValidation(typeof(TaskModel),nameof(ValidateDate))]
         public DateTime due_date { get; set; }
+
         public bool task_staus { get; set; } = false;
         [Required(ErrorMessage ="this Priority level is required")]
         public int PriorityId { get; set; }
         public PriorityModel Priority { get; set; }
+
+        public static ValidationResult ValidateDate(DateTime date, ValidationContext context)
+        {
+            if (date.Date < DateTime.Today)
+            {
+                return new ValidationResult("Date Must be greater than or equal to today's date.");
+            }
+            return ValidationResult.Success;
+        }
     }
 }
